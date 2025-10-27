a31bfafe212df9fe8c61f021ab71324f92329f70
marriedsun <marriedsun@gmail.com>
2025-10-27 17:39:07 +0900

    스타일드컴포넌트 버튼4종

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
+const SimpleButton = styled.button`
+color:white;
+background-color:green;
+`;
+const LargeButton = styled(SimpleButton)`
+  font-size:50px;
+  `;
+  const ReactButton = props => {
+    console.log('props', props);
+    return <button className={props.className}>{props.children}</button>
+  }
+  const ReactLargeButton = styled(ReactButton)`
+    font-size:50px;
+  `;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <SimpleButton>Simple</SimpleButton>
+     <LargeButton>Large</LargeButton>
+     <ReactButton>React</ReactButton>
+     <ReactLargeButton>React Large</ReactLargeButton>
     </div>
   );
 }
